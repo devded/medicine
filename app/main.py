6ecb47a404a7c504451f5b9a1a6ac5bed07788c0
Dedar <thededar@gmail.com>
2024-04-09 21:16:00 +0600

    remove unecessary code

--- app/main.py
+++ app/main.py
@@ -11,42 +11,5 @@ app.include_router(brand.router)
 app.include_router(company.router)
 
 
-# CRUD operations
-# Create (Create)
-
-
-# # Read (GET)
-# @app.get("/generic/{generic_id}")
-# async def read_generic(generic_id: str):
-# 	db = SessionLocal()
-# 	generic = db.query(models.Generic).filter(models.Generic.generic_id == generic_id).first()
-# 	return generic
-
-
-# @app.get("/brands")
-# async def read_brands():
-# 	db = SessionLocal()
-# 	brands = db.query(Brand).all()
-# 	return brands
-
-# @app.get("/brand/{brand_id}")
-# async def read_brand(brand_id: str):
-# 	db = SessionLocal()
-# 	brand = db.query(Brand).filter(Brand.brand_id == brand_id).first()
-# 	return brand
-
-# @app.get("/companies")
-# async def read_companies():
-# 	db = SessionLocal()
-# 	companies = db.query(CompanyName).all()
-# 	return companies
-
-# @app.get("/company/{company_id}")
-# async def read_company(company_id: str):
-# 	db = SessionLocal()
-# 	company = db.query(CompanyName).filter(CompanyName.brand_id == company_id).first()
-# 	return company
-
-
 if __name__ == "__main__":
     uvicorn.run(app)
