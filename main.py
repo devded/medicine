901093396de124405080345d18c06a6046defcf8
Dedar <thededar@gmail.com>
2024-04-09 13:33:32 +0600

    Initial

---
+++ main.py
@@ -0,0 +1,53 @@
+import uvicorn
+from fastapi import FastAPI
+# from sqlalchemy import Column, Integer, String, create_engine
+# from sqlalchemy.ext.declarative import declarative_base
+# from sqlalchemy.orm import sessionmaker
+
+import models
+from database import SessionLocal, engine
+models.Base.metadata.create_all(bind=engine)
+app = FastAPI()
+
+
+
+# CRUD operations
+# Create (Create)
+
+
+# Read (GET)
+@app.get("/generic/{generic_id}")
+async def read_generic(generic_id: str):
+	db = SessionLocal()
+	generic = db.query(models.Generic).filter(models.Generic.generic_id == generic_id).first()
+	return generic
+
+
+# @app.get("/brands")
+# async def read_brands():
+# 	db = SessionLocal()
+# 	brands = db.query(Brand).all()
+# 	return brands
+
+# @app.get("/brand/{brand_id}")
+# async def read_brand(brand_id: str):
+# 	db = SessionLocal()
+# 	brand = db.query(Brand).filter(Brand.brand_id == brand_id).first()
+# 	return brand
+
+# @app.get("/companies")
+# async def read_companies():
+# 	db = SessionLocal()
+# 	companies = db.query(CompanyName).all()
+# 	return companies
+
+# @app.get("/company/{company_id}")
+# async def read_company(company_id: str):
+# 	db = SessionLocal()
+# 	company = db.query(CompanyName).filter(CompanyName.brand_id == company_id).first()
+# 	return company
+
+
+
+if __name__ == "__main__":
+	uvicorn.run(app)
---
+++ models.py
@@ -0,0 +1,84 @@
+from sqlalchemy import Column, Integer, String, create_engine
+from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy.orm import sessionmaker
+from database import Base
+
+class Brand(Base):
+    __tablename__ = 'brand'
+
+    brand_id = Column(String, primary_key=True)
+    generic_id = Column(String)
+    company_id = Column(String)
+    brand_name = Column(String)
+    form = Column(String)
+    strength = Column(String)
+    price = Column(String)
+    packsize = Column(String)
+
+
+class CompanyName(Base):
+    __tablename__ = 'company_name'
+
+    company_id = Column(String, primary_key=True)
+    company_name = Column(String)
+    company_order = Column(String)
+
+
+class Generic(Base):
+    __tablename__ = 'generic'
+
+    generic_id = Column(String, primary_key=True)
+    generic_name = Column(String)
+    precaution = Column(String)
+    indication = Column(String)
+    contra_indication = Column(String)
+    dose = Column(String)
+    side_effect = Column(String)
+    pregnancy_category_id = Column(String)
+    mode_of_action = Column(String)
+    interaction = Column(String)
+
+
+# class Indication(Base):
+#     __tablename__ = 'indication'
+
+#     indication_id = Column(String, primary_key=True)
+#     indication_name = Column(String)
+
+
+# class IndicationGenericIndex(Base):
+#     __tablename__ = 'indication_generic_index'
+
+#     generic_id = Column(String, primary_key=True)
+#     indication_id = Column(String, primary_key=True)
+
+
+# class PregnancyCategory(Base):
+#     __tablename__ = 'pregnancy_category'
+
+#     pregnancy_id = Column(String, primary_key=True)
+#     pregnancy_name = Column(String)
+#     pregnancy_description = Column(String)
+
+
+# class Systemic(Base):
+#     __tablename__ = 'systemic'
+
+#     systemic_id = Column(String, primary_key=True)
+#     systemic_name = Column(String)
+#     systemic_parent_id = Column(String)
+
+
+# class Therapeutic(Base):
+#     __tablename__ = 'therapeutic'
+
+#     therapeutic_id = Column(String, primary_key=True)
+#     therapeutic_name = Column(String)
+#     therapeutic_systemic_class_id = Column(String)
+
+
+# class TherapeuticGeneric(Base):
+#     __tablename__ = 'therapeutic_generic'
+
+#     generic_id = Column(String, primary_key=True)
+#     therapeutic_id = Column(String, primary_key=True)
